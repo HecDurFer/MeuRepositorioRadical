9ed37948ef0dcb4556a1bb1c9c5df9b4ec418659
HecDurFer <97744417+HecDurFer@users.noreply.github.com>
2023-06-21 19:21:46 -0300

    Add files via upload

---
+++ cronometroCorrigido.py
@@ -0,0 +1,50 @@
+import time
+import tkinter as tk
+
+def cronometro(segundos):
+    global cronometro_executando  #correção: adicionar essa linha para atribuir um valor para a variável
+    for i in range(segundos, 0, -1):
+        if not cronometro_executando[0]:
+            return
+        label_tempo["text"] = f"Tempo restante: {i} segundos"
+        janela.update()
+        time.sleep(1)
+    label_tempo["text"] = "Tempo esgotado!"
+
+
+def iniciar_cronometro(event=None):
+    segundos = int(entry_segundos.get())
+    cronometro_executando[0] = True
+    entry_segundos.configure(state="disabled")
+    btn_iniciar.configure(state="disabled")
+    btn_parar.configure(state="normal")
+    cronometro(segundos)
+
+def parar_cronometro():
+    cronometro_executando[0] = False
+    entry_segundos.configure(state="normal")
+    btn_iniciar.configure(state="normal")
+    btn_parar.configure(state="disabled")
+
+cronometro_executando = [False]
+
+janela = tk.Tk()
+janela.title("Cronômetro")
+
+label_instrucao = tk.Label(janela, text="Digite a quantidade de segundos:")
+label_instrucao.pack()
+
+entry_segundos = tk.Entry(janela)
+entry_segundos.pack()
+entry_segundos.bind('<Return>', iniciar_cronometro)
+
+btn_iniciar = tk.Button(janela, text="Iniciar", command=iniciar_cronometro)
+btn_iniciar.pack()
+
+btn_parar = tk.Button(janela, text="Parar", command=parar_cronometro, state="disabled")
+btn_parar.pack()
+
+label_tempo = tk.Label(janela, text="")
+label_tempo.pack()
+
+janela.mainloop()
